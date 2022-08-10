cb7826f29ca1a0290fa6defb11a16e5fa7cbdb28
Vitaly <vitalijskrumins@gmail.com>
2022-08-10 09:33:43 +0200

    Level loader

---
+++ LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gravity
+{
+    public static class LevelLoader
+    {
+        public Level Load(Texture2D data)
+        {
+            var pixels = new Color[data.Width * data.Height];
+            data.GetData(pixels);
+
+            // Store pixels into a two-dimensional array.
+            var levelData = new Color[levelMap.Width, levelMap.Height];
+            for (int y = 0; y < levelMap.Height; y++)
+            {
+                for (int x = 0; x < levelMap.Width; x++)
+                {
+                    levelData[x, y] = pixels[x + y * levelMap.Width];
+                }
+            }
+        }
+    }
+}
